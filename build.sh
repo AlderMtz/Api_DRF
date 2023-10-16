1f7403a3f663992f84c8bf1d7088ad192e4ee84d
Alder Martinez <alder.mtz.ant@gmail.com>
2023-10-15 21:37:12 -0600

    correccion de tipeado

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install requirements.txt #la lista de requisitos necesarios para que funncione la API
+pip install -r requirements.txt #la lista de requisitos necesarios para que funncione la API
 
 python manage.py collectstatic --no-input
 python manage.py migrate
