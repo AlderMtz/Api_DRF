f65e9ff22e48e38b91a664ab483f9fb311e3de9d
Alder Martinez <alder.mtz.ant@gmail.com>
2023-10-15 21:30:07 -0600

    modificacion del archivo build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install requirements.txt #la lista de requisitos necesarios para que funncione la API
 
 python manage.py collectstatic --no-input
